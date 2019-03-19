43f6020fdacf3501e660ec3d3f8f508bbc0c5b83
GameFozerYT <48291236+GameFozerYT@users.noreply.github.com>
2019-03-19 22:58:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "$";
-var adminprefix = 'st'
+var adminprefix = '$'
 
 
-const developers = ["id"]
+const developers = ["488028699845722144"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
